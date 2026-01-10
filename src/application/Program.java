8680dd9c774b01eae8f1be96e3ed51de75f48026
pierry-savio <pierrysavio.pi@gmail.com>
2026-01-10 15:06:12 -0300

    feat: add main class Program

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println(" _ _ _   _ _ _   _ _ _");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println(" _ _ _   _ _ _   _ _ _");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println(" _ _ _   _ _ _   _ _ _");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+		System.out.println("|_|_|_| |_|_|_| |_|_|_|");
+	}
+}
